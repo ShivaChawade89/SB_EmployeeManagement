0e8f7533f0536f3e0501f274586c40e76965e34f
shiv kumar chawade <shvachawade89@mail.com>
2023-09-05 14:19:22 +0530

    1st commit

--- src/main/java/com/shiv/SB_EmployeeManagement/repo/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.shiv.SB_EmployeeManagement.repo;
-
-import com.shiv.SB_EmployeeManagement.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepo extends JpaRepository<Employee,Integer> {
-}
